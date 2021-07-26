4433c023081bcb00893382b44e5abaa9986903c1
ShadatRahman <shadat.rahman.464@gmail.com>
2021-07-26 14:34:48 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+const switcher = document.querySelector('.themebtn');
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+    console.log('Current class name: ' + className);
+
+});
